f9548df6f3a58ecaac70ff2679437bee44acd5e2
717785320 <40885345+717785320@users.noreply.github.com>
2019-07-22 14:03:44 +0800

    Add files via upload

---
+++ js/myapp.js
@@ -0,0 +1,34 @@
+﻿var myApplist = [
+//{ "name": "Thor 1.2.2", "title": "shaynelegend@outlook.com", "plist": "https://raw.githubusercontent.com/717785320/thor/master/Thor1.2.2.plist", "img": "https://raw.githubusercontent.com/717785320/thor/master/AppIcon83.5x83.5%402x~ipad.png" },
+
+//{ "name": "Shadowrocket-2.1.10", "title": "(PP正版)", "plist": "https://raw.githubusercontent.com/717785320/shadowrocket/master/Shadowrocket-2.1.10.plist", "img": "https://github.com/717785320/shadowrocket/blob/master/20187111315435117.jpg?raw=true" },
+
+//{ "name": "Shadowrocket-2.1.23", "title": "shaynelegend2@outlook.com", "plist": "https://raw.githubusercontent.com/717785320/Shadowrocket-2.1.23/master/Shadowrocket-2.1.23.plist", "img": "https://github.com/717785320/shadowrocket/blob/master/20187111315435117.jpg?raw=true" },
+
+{ "name": "Shadowrocket-2.1.23", "title": "4049", "plist": "https://raw.githubusercontent.com/717785320/MyShadowrocket/master/shadowrocket2.1.23.plist", "img": "https://github.com/717785320/shadowrocket/blob/master/20187111315435117.jpg?raw=true" },
+
+{ "name": "Shadowrocket-2.1.30", "title": "4049", "plist": "https://raw.githubusercontent.com/717785320/MyShadowrocket/master/shadowrocket2.1.30.plist", "img": "https://github.com/717785320/shadowrocket/blob/master/20187111315435117.jpg?raw=true" },
+
+{ "name": "Shadowrocket-2.1.23", "title": "7177", "plist": "https://raw.githubusercontent.com/717785320/Shadowrocket-2.1.23-my-/master/Shadowrocket-2.1.23.plist", "img": "https://github.com/717785320/shadowrocket/blob/master/20187111315435117.jpg?raw=true" },
+
+{ "name": "寒梅1.3.1", "title": "4049", "plist": "https://raw.githubusercontent.com/717785320/Mume/master/mume.plist", "img": "https://github.com/717785320/Mume/blob/master/mume.png?raw=true" },
+
+{ "name": "熊猫吃短信 1.4.5", "title": "7177", "plist": "https://raw.githubusercontent.com/717785320/XiongMaoChiDuanXin/master/panda.plist", "img": "https://github.com/717785320/XiongMaoChiDuanXin/blob/master/20190313104156.png?raw=true" },
+
+{ "name": "植物大战僵尸 ", "title": "4049", "plist": "https://raw.githubusercontent.com/717785320/Plants-vs.-Zombies/master/PlantsVsZombies_v1.9.13.plist", "img": "https://github.com/717785320/Plants-vs.-Zombies_v1.9.13/blob/master/20190314150040.png?raw=true" },
+
+{ "name": "微信 6.6.1", "title": "7177", "plist": "https://raw.githubusercontent.com/717785320/WeChat/master/wechat6.6.1.plist", "img": "https://github.com/717785320/WeChat6.7.4/blob/master/3336519328.jpg?raw=true" },
+
+{ "name": "微信 6.7.4", "title": "7177", "plist": "https://raw.githubusercontent.com/717785320/WeChat/master/wechat6.7.4.plist", "img": "https://github.com/717785320/WeChat6.7.4/blob/master/3336519328.jpg?raw=true" },
+
+
+{ "name": "百度网盘8.11.1", "title": "可抓包版", "plist": "https://raw.githubusercontent.com/717785320/BaiduNetdisk/master/baidudisk8.11.1.plist", "img": "https://github.com/717785320/BaiduNetdisk/blob/master/11111111111111111.png?raw=true" }
+
+
+];
+
+var apkApplist = [
+
+{ "name": "Kitsunebi 1.6.17", "title": "apk", "plist": "https://github.com/717785320/kitsunebi/releases/download/v1.6.17/kitsunebi.apk", "img": "https://github.com/717785320/kitsunebi/blob/master/20190715141450.jpg?raw=true" }
+
+];
