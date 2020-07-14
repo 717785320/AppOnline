cc15f3d1554986abdaf50fc9b46424934b0a5101
717785320 <40885345+717785320@users.noreply.github.com>
2020-07-14 13:12:28 +0800

    Update loon_dylike.js

--- loon_dylike.js
+++ loon_dylike.js
@@ -3,18 +3,18 @@ console.log("dylike脚本开始!");
 var url = $request.url;
 var headers = $request.headers;
 var body = $request.body;
+var likeNum = 16;
 try {
-    if (url.indexOf("//webcast3-normal-c-hl.amemv.com/webcast/room/like/?") >= 0) {
+    if (url.indexOf("//webcast3-normal-c-hl.amemv.com/webcast/room/like/?") > -1) {
 
-        if (!!body) {
+        if (!!body && body.indexOf("count") > -1 && body.indexOf("room_id") > -1) {
 
-            //$notification.post('获取到手动点赞!', '获取到手动点赞!', body)
+            $notification.post('获取到手动点赞!', '开始执行自动点赞!', body)
 
             console.log("###########################################");
             console.log("原body:" + body);
 
-            //body = body.replace("count=3", "count=");
-            body = "count=12&" + body.split('&')[1];
+            body = "count=" + likeNum + "&" + body.split('&')[1];
 
             console.log("修改后body:" + body);
 
@@ -30,19 +30,30 @@ try {
             console.log("请求体:" + JSON.stringify(params));
             console.log("请求体****************************************");
 
-            var likeCount = 12;
+            var likeCount = likeNum;
             function forPost() {
                 setTimeout(function () {
+
                     console.log("循环请求执行");
+
                     $httpClient.post(params, function (error, response, data) {
+
                         console.log("错误:" + error);
                         console.log("返回数据:" + data);
-                        if (data.indexOf("手速") > -1) {
+
+                        if (data.indexOf("手速太快了") > -1) {
+
                             $notification.post('点赞已到上限!', '点赞个数:' + likeCount, data);
                             console.log("点赞已上限,个数:" + likeCount + ";" + data);
                         }
+                        else if (data.indexOf("请登录") > -1) {
+
+                            $notification.post('请登录!', '请登录:', data);
+                            console.log("请登录;" + data);
+                        }
                         else {
-                            likeCount += 12;
+
+                            likeCount += likeNum;
                             forPost();
                         }
                     });
@@ -57,6 +68,5 @@ try {
     console.log("try错误:" + e);
 }
 
-$done({body});
-//$done(body);
+$done({});
 console.log("执行完成!!!!");
